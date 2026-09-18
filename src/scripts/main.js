fd34ce64c71f54c041687b20a3752304c081dae7
DouglasLopesDRL <douglasribeirodrl@gmail.com>
2026-09-17 21:27:12 -0300

    criado toda a lógica de js usando jquery e o fetch. Feito alguns ajustes no html criando ids para facilitar o uso no js.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('Olá Mundo!');
+$(document).ready(function() {
+    const endpoint = 'https://api.github.com/users/DouglasLopesDRL';
+
+    // Consumindo API do GitHub
+    fetch(endpoint)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            $('#name').text(json.name);
+            $('#username').text(`@${json.login}`);
+            $('#avatar').attr('src', json.avatar_url);
+
+            // Seleciona os itens da lista mantendo o título <h4>
+            $('#repositories').html(`<h4>Repositórios</h4> ${json.public_repos}`);
+            $('#followers').html(`<h4>Seguidores</h4> ${json.followers}`);
+            $('#following').html(`<h4>Seguindo</h4> ${json.following}`);
+
+            // Atualiza o link do perfil
+            $('#github').attr('href', json.html_url);
+        })
+        .catch(function(error) {
+            console.error('Erro ao buscar dados do GitHub:', error);
+        });
+});
